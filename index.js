86c998a7fe8eb6c189b16dc342161f43d98afdc4
Shukri Adams <shukri.adams@gmail.com>
2020-09-27 12:50:41 +0200

    add shim on error
    if log writing is not possible, the wrapper will expose a shim of winston allowing calls to the log to continue without exceptions being raised. Logging errors should never raise exceptions of their own.

--- index.js
+++ index.js
@@ -2,45 +2,64 @@ let winston = require('winston'),
     fs = require('fs'),
     _instance,
     process = require('process'),
-    path = require('path');
-
+    path = require('path')
 
 class Logger {
     constructor(logFolder){
 
         if (!logFolder)
-            logFolder = path.join(process.cwd(), '__logs');
+            logFolder = path.join(process.cwd(), '__logs')
 
-        if (!fs.existsSync(logFolder))
-            fs.mkdirSync(logFolder);
+        let canWrite = true
+        if (!fs.existsSync(logFolder)) {
+            try {
+                fs.mkdirSync(logFolder)
+            } catch (ex){
+                console.log(ex)
+                canWrite = false
+            }
+        }
 
         // apply rotation override for winston
-        require('winston-daily-rotate-file');
+        require('winston-daily-rotate-file')
 
-        this.info = new (winston.Logger)({
-            transports: [
-                new (winston.transports.DailyRotateFile)({
-                    filename: path.join(logFolder, '.log'),
-                    datePattern: 'info.yyyy-MM-dd',
-                    prepend: true,
-                    level: 'info'
-                }),
-                new (winston.transports.Console)()
-            ]
-        });
-
-        this.error = new (winston.Logger)({
-            transports: [
-                new (winston.transports.DailyRotateFile)({
-                    filename: path.join(logFolder, '.log'),
-                    datePattern: 'error.yyyy-MM-dd',
-                    prepend: true,
-                    level: 'error'
-                }),
-                new (winston.transports.Console)()
-            ]
-
-        });
+        if (canWrite){
+            this.info = new (winston.Logger)({
+                transports: [
+                    new (winston.transports.DailyRotateFile)({
+                        filename: path.join(logFolder, '.log'),
+                        datePattern: 'info.yyyy-MM-dd',
+                        prepend: true,
+                        level: 'info'
+                    }),
+                    new (winston.transports.Console)()
+                ]
+            })
+    
+            this.error = new (winston.Logger)({
+                transports: [
+                    new (winston.transports.DailyRotateFile)({
+                        filename: path.join(logFolder, '.log'),
+                        datePattern: 'error.yyyy-MM-dd',
+                        prepend: true,
+                        level: 'error'
+                    }),
+                    new (winston.transports.Console)()
+                ]
+            })
+    
+        } else {
+            // writing not possible, expose shim of winston interface.
+            this.info = {
+                info : function(){},
+                error : function(){}
+            }
+    
+            this.error = {
+                info : function(){},
+                error : function(){}
+            }
+        }
 
     }
 }
@@ -49,7 +68,7 @@ module.exports = {
     
     // initializes logger with path to log to, call this once in app lifetime if needed
     initialize : function(logpath){
-        _instance = new Logger(logpath);
+        _instance = new Logger(logpath)
     },
     
     // returns a new instance of logger per call
@@ -62,13 +81,13 @@ module.exports = {
 
         // if logger not initialized creates logger with default options
         if (!_instance) 
-            _instance = new Logger(logpath);
+            _instance = new Logger(logpath)
 
-        return _instance;
+        return _instance
     },
 
     // allow injection of external logger, this is useful for testing
     set : function(newInstance){
-        _instance = newInstance;
+        _instance = newInstance
     }
-};
+}
